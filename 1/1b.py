b45c9efea8a2ef02fd02c5c2b3f01aca42b29335
logan <logan.orr@groundtruth.co.nz>
2018-12-03 10:38:48 +1300

    Benchmarked my first take on 1b, then made a couple minor changes to improve performance Improvement 60seconds to 0.04

--- 1/1b.py
+++ 1/1b.py
@@ -1,6 +1,8 @@
+from timeit import Timer
+
 print("Advent 1.b")
 
-inputFile = open('test-input-answer-10.TXT', 'r')
+inputFile = open('input.txt', 'r')
 inputList = inputFile.read().strip().split()
 inputFile.close()
 
@@ -10,15 +12,37 @@ def findRepeat():
 	frequencyList = []
 	
 	while (True):
-		print("*")
 		for change in inputList:
 			currentFrequency += int(change)
 
 			if currentFrequency in frequencyList:
+				print("The first repeated frequency %d" % currentFrequency)
 				return currentFrequency
 			else:
 				frequencyList.append(currentFrequency)
 
 
+print("Naive bruteforce approach\n----")
+t = Timer("findRepeat()", "from __main__ import findRepeat")
+print("Time: %s\n----------------" % t.timeit(number=1)) # 62.72738941095304
 
-print("The first repeated frequency %d" % findRepeat())
+
+def findRepeatUsingSets():
+	currentFrequency = 0;
+	previousSetSize = 0;
+	frequencyList = set()
+	
+	while (True):
+		for change in inputList:
+			currentFrequency += int(change)
+			frequencyList.add(currentFrequency)
+
+			if len(frequencyList) == previousSetSize:
+				print("The first repeated frequency %d" % currentFrequency)
+				return currentFrequency
+
+			previousSetSize = len(frequencyList)
+
+print("Using sets instead, exploiting that the set size will not increase if it receives a duplicate\n----")
+t2 = Timer("findRepeatUsingSets()", "from __main__ import findRepeatUsingSets")
+print("Time: %s \n----------------" % t2.timeit(number=1)) # 0.04230022802948952
