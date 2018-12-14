0e9a93998c6a3666cb9e68e0a57e59d7e1e7e42b
logan <logan.orr@groundtruth.co.nz>
2018-12-14 15:10:26 +1300

    Very minor optimization to day 11

--- 11/sol11b4Optimal.py
+++ 11/sol11b4Optimal.py
@@ -17,11 +17,11 @@ class Solution(object):
         grid = defaultdict(list)
 
         for x in range(0, 300):
-            grid[x] = [self.calculateCoods(serial, x, y) for y in range(0, 300)]
+            [self.calculateCell(serial, grid, x, y) for y in range(0, 300)]
 
 
-        for x in range(0, 300):
-            [self.calculateCellSummedAreaTable(grid, x, y) for y in range(0, 300)]
+        #for x in range(0, 300):
+        #    [self.calculateCellSummedAreaTable(grid, x, y) for y in range(0, 300)]
 
         highestTotal = -100000
         coordsOfHighest = 0
@@ -42,8 +42,13 @@ class Solution(object):
 
         return coordsOfHighest
         
+    def calculateCell(self, serial, grid, x, y):
+        self.calculateCoods(grid, serial, x, y)
+        self.calculateCellSummedAreaTable(grid, x, y)
+    
     def calculateCellSummedAreaTable(self, grid, x,  y):
         value = grid[x][y]
+        
         if y - 1 >= 0:
             value += grid[x][y-1]
             
@@ -67,7 +72,7 @@ class Solution(object):
 
 
 
-    def calculateCoods(self, serial, x, y):
+    def calculateCoods(self, grid, serial, x, y):
         rackId = x + 10
         powerlevel = ((rackId * y) + serial) * rackId
 
@@ -76,7 +81,7 @@ class Solution(object):
         if (len(levelAsString) >= 3):
             powerlevel = int(levelAsString[-3])
 
-        return powerlevel - 5
+        grid[x].append(powerlevel - 5)
 
 
     def run(self):
